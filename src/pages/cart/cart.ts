28eeb4260153ba22f21ed5ed4a99d69008e43744
Thiago Barbosa Soares <thiagobarso@gmail.com>
2019-05-13 14:16:31 -0300

    aula 139

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -4,6 +4,7 @@ import { CartItem } from '../../models/cart-item';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
 import { CartService } from '../../services/domain/cart.service';
+import { ProdutoDTO } from '../../models/produto.dto';
 
 @IonicPage()
 @Component({
@@ -17,12 +18,12 @@ export class CartPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public cartSercive: CartService,
+    public cartService: CartService,
     public produtoService: ProdutoService) {
   }
 
   ionViewDidLoad() {
-    let cart = this.cartSercive.getCart();
+    let cart = this.cartService.getCart();
     this.items = cart.items;
     this.loadImageUrls();
   }
@@ -35,6 +36,26 @@ export class CartPage {
         item.produto.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.produto.id}-small.jpg`;
       },error=>{});
     }
+  }
+
+  removeItem(produto : ProdutoDTO) {
+    this.items = this.cartService.removeProduto(produto).items;
+  }
+
+  increaseQuantity(produto : ProdutoDTO){
+    this.items = this.cartService.increaseQuantity(produto).items;
+  }
+
+  decreaseQuantity(produto : ProdutoDTO){
+    this.items = this.cartService.decreaseQuantity(produto).items;
+  }
+
+  total() : number {
+    return this.cartService.total();
+  }
+
+  goOn(){
+    this.navCtrl.setRoot('CategoriasPage');
   }
 
 }
