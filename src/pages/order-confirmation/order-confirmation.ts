08b4acc1f23eef0ec889a6bddcc4f26a37797b15
Thiago Barbosa Soares <thiagobarso@gmail.com>
2019-05-15 16:54:08 -0300

    aula 149

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
 
   constructor(
     public navCtrl: NavController,
@@ -56,7 +57,7 @@ export class OrderConfirmationPage {
     this.pedidoService.insert(this.pedido)
       .subscribe(response => {
         this.cartService.createOrClearCart();
-        console.log(response.headers.get('location'));
+        this.codpedido = this.extractId(response.headers.get('location'));
       }, error => {
         if (error.status == 403) {
           this.navCtrl.setRoot('HomePage');
@@ -64,7 +65,16 @@ export class OrderConfirmationPage {
       });
   }
 
-  back(){
+  back() {
     this.navCtrl.setRoot('CartPage');
+  }
+
+  private extractId(location: string): string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
+  }
+
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
   }
 }
