06b0dc393964f6f2f81cb451c6d72679ea40345a
Thiago Barbosa Soares <thiagobarso@gmail.com>
2019-05-19 15:09:34 -0300

    aula 153

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -4,13 +4,7 @@ import { StorageService } from '../../services/storage.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { API_CONFIG } from '../../config/api.config';
-
-/**
- * Generated class for the ProfilePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @IonicPage()
 @Component({
@@ -20,12 +14,15 @@ import { API_CONFIG } from '../../config/api.config';
 export class ProfilePage {
 
   cliente : ClienteDTO;
+  picture : string;
+  cameraOn : boolean = false;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService : ClienteService) {
+    public clienteService : ClienteService,
+    public camera : Camera) {
   }
 
   ionViewDidLoad() {
@@ -52,6 +49,25 @@ export class ProfilePage {
       this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
     },
     error => {});
+  }
+
+  getCameraPicture() {
+
+    this.cameraOn = true;
+
+    const options : CameraOptions = {
+      quality: 100,
+      destinationType : this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.picture = 'data:image/png;base64,' + imageData;
+      this.cameraOn = false;
+    }, (err) =>{
+
+    });
   }
 
 }
